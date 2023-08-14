4709476d78c78ecfad5d6162ff4eca3f5f4bde03
snowborodist@gmail.com <snowbro@MBP-Aleksandr.dynreal.starlink.ru>
2023-08-14 23:53:23 +0300

    fix test_user and pasword in test data

--- tests/functional/test_data/db_data.py
+++ tests/functional/test_data/db_data.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 PasswordAndHash = namedtuple('PasswordAndHash', 'password hash')
 
 test_password_and_hash_pair = PasswordAndHash('hackme',
-                                              r'\xf59a33e80babb1d53516302590fa7ff123f9ed533a54c'
+                                              r'f59a33e80babb1d53516302590fa7ff123f9ed533a54c'
                                               r'cab565924e84c8d271f46b4902a1a8fe21392e43e6de6c1'
                                               r'c21de91399ca73bdd3bed0b66dc14ec96c00')
 
