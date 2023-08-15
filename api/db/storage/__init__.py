1e60cbac11871e3e601c52c771559361df57289d
snowborodist@gmail.com <snowbro@MBP-Aleksandr.dynreal.starlink.ru>
2023-08-15 19:41:00 +0300

    fix imports in api.db.storage module

--- api/db/storage/__init__.py
+++ api/db/storage/__init__.py
@@ -2,7 +2,9 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from db.storage.abstract_storage import AbstractStorage
+from db.storage.abstract_storage import AbstractStorage, NestedObjectFilter
 from db.storage.elastic_storage import ElasticStorage
 
 StorageDep = Annotated[AbstractStorage, Depends(ElasticStorage)]
+
+__all__ = ['StorageDep', NestedObjectFilter, AbstractStorage, ElasticStorage]
