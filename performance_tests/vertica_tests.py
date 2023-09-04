e8c543a67cdfa9ce341a85e1ff90ff52ad77db0d
Alexander Safonov <snowborodist@yandex.ru>
2023-09-04 22:48:28 +0300

    remove comments

--- performance_tests/vertica_tests.py
+++ performance_tests/vertica_tests.py
@@ -95,55 +95,5 @@ class VerticaTests(TestBase):
                 'avg_seconds': t_sum / n
             }}
 
-    # def test_read_mp(self, cores: int = 4, n: int = 10) -> dict[str, any]:
-    #     with mp.Pool(cores) as pool:
-    #         results = pool.map(self.test_read, [n for _ in range(cores)])
-    #         avg_seconds = sum([test['test_read']['avg_seconds'] for test in results]) / len(results)
-    #         return {'test_read_mp': {
-    #             'operation': READ_QUERY,
-    #             'cores': cores,
-    #             'iterations': n,
-    #             'avg_seconds': avg_seconds
-    #         }}
-    #
-    # def test_write_mp(self, chunk_file: str, cores: int = 4, n: int = 4) -> dict[str, any]:
-    #     with mp.Pool(cores) as pool:
-    #         results = pool.starmap(self.test_write, [(chunk_file, n) for _ in range(cores)])
-    #         avg_seconds = sum([test['test_write']['avg_seconds'] for test in results]) / len(results)
-    #         return {'test_write_mp': {
-    #             'chunk_file': chunk_file,
-    #             'cores': cores,
-    #             'iterations': n,
-    #             'avg_seconds': avg_seconds
-    #         }}
-    #
-    # def test_mixed_mp(self, chunk_file: str, cores: int = 4, n: int = 10) -> dict[str, any]:
-    #     with mp.Pool(cores) as pool:
-    #         args = []
-    #         for c_idx in range(cores):
-    #             args.append(('read' if c_idx % 2 else 'write', chunk_file, n))
-    #         results = pool.starmap(self._work, args)
-    #
-    #         _r_cnt = _w_cnt = 0
-    #         avg_read_seconds = avg_write_seconds = 0.
-    #         for res in results:
-    #             if read_res := res.get('test_read'):
-    #                 _r_cnt += 1
-    #                 avg_read_seconds += read_res.get('avg_seconds')
-    #             elif write_res := res.get('test_write'):
-    #                 _w_cnt += 1
-    #                 avg_write_seconds += write_res.get('avg_seconds')
-    #         avg_read_seconds /= _r_cnt
-    #         avg_write_seconds /= _w_cnt
-    #
-    #         return {'test_mixed_mp': {
-    #             'read_operation': self.READ_QUERY,
-    #             'write_chunk_file': chunk_file,
-    #             'cores': cores,
-    #             'iterations': n,
-    #             'avg_read_seconds': avg_read_seconds,
-    #             'avg_write_seconds': avg_write_seconds
-    #         }}
-
 
 __all__ = ['VerticaTests']
