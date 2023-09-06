407059db141640b8f6e9f79ccc0b917ce64f6727
Alexander Safonov <snowborodist@yandex.ru>
2023-09-06 22:06:55 +0300

    fix clickhouse migration

--- deploy/clickhouse_initial/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wait-for-it --service http://localhost:8123/ \
-            -- python initial.py
--- deploy/clickhouse_initial/initial.py
+++ deploy/clickhouse_initial/initial.py
@@ -1,15 +1,15 @@
 from clickhouse_driver import Client
 
 
-client = Client(host='clickhouse-node1')
-client.execute('CREATE DATABASE IF NOT EXISTS example ON CLUSTER company_cluster')
+client = Client(host='clickhouse1')
+client.execute('CREATE DATABASE IF NOT EXISTS example;')
 client.execute(
-    '''CREATE TABLE example.regular_table ON CLUSTER company_cluster 
+    '''CREATE TABLE example.regular_table
         (
             id Int64, 
             film UUID,
             user UUID,
             viewed_seconds Int32,
-            created DateTime('Europe/Moscow'),
-        ) Engine=MergeTree() ORDER BY id'''
+            created DateTime('Europe/Moscow')
+        ) Engine=MergeTree() ORDER BY id;'''
 )
