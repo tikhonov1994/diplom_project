82e624f7f1c49f7d35d2fd38b61ad99a5ef1c979
snowborodist@gmail.com <snowbro@MBP-Aleksandr.dynreal.starlink.ru>
2023-09-10 21:56:25 +0300

    add batch buffering in clickhouse loader

--- ugc_etl/src/loader.py
+++ ugc_etl/src/loader.py
@@ -1,5 +1,6 @@
 from clickhouse_driver import Client
 from clickhouse_driver.errors import NetworkError
+from time import monotonic
 
 from src.core.config import app_config
 from src.models import ViewsMessage
@@ -12,6 +13,8 @@ class ClickhouseViewsLoader:
         self._client = Client(host=app_config.clickhouse.host,
                               port=app_config.clickhouse.port)
         self.migrate()
+        self._batch_buffer: list[dict[str, any]] = []
+        self._batch_time: float = monotonic()
 
     @backoff(exceptions=(NetworkError, EOFError))
     def migrate(self):
@@ -30,5 +33,18 @@ class ClickhouseViewsLoader:
 
     @backoff(exceptions=(NetworkError, EOFError))
     def add_message(self, msg: ViewsMessage) -> None:
-        self._client.execute('INSERT INTO ugc.views SETTINGS async_insert=1, wait_for_async_insert=1 VALUES',
-                             [msg.dict(), ])
+        self._batch_buffer.append(msg.dict())
+
+        if len(self._batch_buffer) > app_config.clickhouse.insert_batch_size \
+                or (monotonic() - self._batch_time) > app_config.clickhouse.insert_batch_timeout_sec:
+
+            if not self._batch_buffer:
+                logger.info('No new items for clickhouse.')
+                self._batch_time = monotonic()
+                return
+
+            self._client.execute('INSERT INTO ugc.views SETTINGS async_insert=1, wait_for_async_insert=1 VALUES',
+                                 self._batch_buffer)
+            logger.info('Loader buffer flush: pushed %d items to clickhouse.', len(self._batch_buffer))
+            self._batch_time = monotonic()
+            self._batch_buffer.clear()
