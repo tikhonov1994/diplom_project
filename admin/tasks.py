28418b5b0090558c96a0de8e041caac542cce83c
tikhonov199 <tikhonov199@yandex.ru>
2023-10-13 21:18:50 +0400

    commit 2

--- admin/tasks.py
+++ admin/tasks.py
@@ -17,6 +17,13 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     sender.add_periodic_task(10.0, send_messages.s([Mailing.objects.first().pk,
+#                                                     {'params': {'subject': 'hello'}}]),
+#                                                     name='send every 10')
+
+
 @shared_task(name='send_messages')
 def send_messages(mailing_id: str, params: dict):
     mailing = Mailing.objects.get(id=UUID(mailing_id))
@@ -24,6 +31,6 @@ def send_messages(mailing_id: str, params: dict):
         'template_id': mailing.template.pk,
         'template_params': params,
         'recipients_list': mailing.users_ids,
-        'subject': None
+        'subject': params.get('subject')
     }
     requests.post('notification_api/notification_api/api/v1/mailing/send', data=data)
