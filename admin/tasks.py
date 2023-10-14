3c033d9ec83c7888302fbe4310b6ad9643fa1d4b
Alexander Safonov <snowborodist@yandex.ru>
2023-10-14 16:07:40 +0300

    fix requests calls

--- admin/tasks.py
+++ admin/tasks.py
@@ -39,20 +39,23 @@ def send_messages(mailing_id: str, params: dict):
 @shared_task(name='notify_new_popular_reviews')
 def notify_new_popular_reviews(mailing_id: str, params: dict):
     mailing = Mailing.objects.get(id=UUID(mailing_id))
-    with requests.get(
-            'social_api:8010/social_api/api/v1/reviews/daily-top-reviews') as reviews_response:
-        reviews_response.raise_for_status()
-        reviews: list[dict[str, any]] = reviews_response.json()
-        data = {
-            'template_id': mailing.template.pk,
-            'template_params': params,
-            'recipients_list': mailing.users_ids,
-            'subject': params.get('subject')
-        }
-        for i in range(len(reviews)):
-            with requests.get(f'api:8001/content/api/v1/films/{reviews[i]["film_id"]}') as film_response:
-                film_response.raise_for_status()
-                film_data = film_response.json()
-                reviews[i]['film_title'] = film_data['title']
-        data['template_params']['reviews'] = reviews
-        requests.post('notification_api:8005/notification_api/api/v1/mailing/send', data=data)
+
+    reviews_response = requests.get('social_api:8010/social_api/api/v1/reviews/daily-top-reviews')
+    reviews_response.raise_for_status()
+    reviews: list[dict[str, any]] = reviews_response.json()
+
+    data = {
+        'template_id': mailing.template.pk,
+        'template_params': params,
+        'recipients_list': mailing.users_ids,
+        'subject': params.get('subject')
+    }
+
+    for i in range(len(reviews)):
+        film_response = requests.get(f'api:8001/content/api/v1/films/{reviews[i]["film_id"]}')
+        film_response.raise_for_status()
+        film_data = film_response.json()
+        reviews[i]['film_title'] = film_data['title']
+
+    data['template_params']['reviews'] = reviews
+    requests.post('notification_api:8005/notification_api/api/v1/mailing/send', data=data)
