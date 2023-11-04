979838a58f7276c08c048b634d74774cd8ec1dfd
tikhonov199 <tikhonov199@yandex.ru>
2023-11-04 23:26:43 +0700

    + commit

--- admin/movies/admin.py
+++ admin/movies/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
 
 from .models import Filmwork, GenreFilmwork, Person, PersonFilmwork, User
 
