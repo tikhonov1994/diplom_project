9ddbc5c274632a275e907005a12b3d83fda12264
ambush9 <ambush2626@gmail.com>
2023-11-05 14:51:21 +0300

    change schema to public

--- user/services/__init__.py
+++ user/services/__init__.py
@@ -1,4 +1,4 @@
 from services.images import ImagesServiceDep
 from services.user_profile import UserProfileServiceDep
 
-__all__ = ['ImagesServiceDep', 'UserProfileService']
+__all__ = ['ImagesServiceDep', 'UserProfileServiceDep']
