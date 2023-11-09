43d3d9718ee68aaa52d569a72583c8f720d6e058
Alexander Safonov <snowborodist@yandex.ru>
2023-11-09 15:40:03 +0300

    fix env example and minio_fixtures

--- tests/functional/minio_fixtures.py
+++ tests/functional/minio_fixtures.py
@@ -6,11 +6,12 @@ from settings import test_settings
 
 
 @pytest.fixture(scope='session', autouse=True)
-def clean_minio() -> None:
+def purge_minio() -> None:
     client = Minio(f'{test_settings.minio_host}:{test_settings.minio_port}',
                    test_settings.minio_root_user,
                    test_settings.minio_root_password,
                    secure=False)
-    images_to_delete = client.list_objects(test_settings.user_minio_image_bucket)
-    for img in images_to_delete:
-        client.remove_object(test_settings.user_minio_image_bucket, img.object_name)
+    if client.bucket_exists(test_settings.user_minio_image_bucket):
+        images_to_delete = client.list_objects(test_settings.user_minio_image_bucket)
+        for img in images_to_delete:
+            client.remove_object(test_settings.user_minio_image_bucket, img.object_name)
